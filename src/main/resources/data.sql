f3496c7236876e912f40156b1a12719b09b08868
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-11-23 10:03:22 -0300

    deleted delivery  database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,17 +7,3 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 -- INSERT INTO orders (id_order, id_client, id_parcel, total_value, order_address)
 -- VALUES (1, 123, 456, 1.99, 'Rua de cima');
-
--- CREATE TABLE IF NOT EXISTS ordersTest (
--- 	id_order VARCHAR(36) NOT NULL PRIMARY KEY,
--- 	id_client INT NOT NULL,
--- 	id_parcel INT NOT NULL,
--- 	total_value FLOAT NOT NULL,
--- 	order_address VARCHAR(256) NOT NULL
--- );
-
-CREATE TABLE IF NOT EXISTS Delivery (
-	id_delivery VARCHAR NOT NULL PRIMARY KEY,
-	id_client VARCHAR NOT NULL,
-	order_address VARCHAR NOT NULL
-);
